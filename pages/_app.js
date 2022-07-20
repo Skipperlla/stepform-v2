0d7043b08dbf50b850c066384bb9834599b5d169
Skipperlla <oesmer437@gmail.com>
2022-07-21 00:02:05 +0300

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+//STYLES
+import "../styles/globals.scss";
+import "react-phone-input-2/lib/style.css";
+import theme from "../theme";
+import { ThemeProvider } from "@material-ui/styles";
+
+import PropTypes from "prop-types";
+import "../i18nextConf";
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
+library.add(fas);
+
+function MyApp(props) {
+  const { Component, pageProps } = props;
+
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+  const getData = async () => {
+    const res = await axios.get("https://geolocation-db.com/json/");
+
+    localStorage.setItem("geolocation", JSON.stringify(res.data));
+  };
+
+  React.useEffect(() => {
+    getData();
+  }, []);
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default MyApp;
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
